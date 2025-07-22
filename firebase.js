d4ee3fba727e4cb458ab73524495d47f47509058
emmadam02 <emmadam02@gmail.com>
2025-07-22 12:43:37 +0100

    first commit

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+const firebaseConfig = {
+    apiKey: "AIzaSyAJsI_r80ZRqG80jxss5dQcvGFs0G6w-Tc",
+    authDomain: "jumia-8f827.firebaseapp.com",
+    projectId: "jumia-8f827",
+    storageBucket: "jumia-8f827.firebasestorage.app",
+    messagingSenderId: "506048228924",
+    appId: "1:506048228924:web:37d104efade80ba6225d39"
+};
+
+
+export const app = initializeApp(firebaseConfig)
